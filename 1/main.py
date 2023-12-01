a4f4dcf0bd944b84dd28e0394483ebd64426c6af
Mikołaj Lubiak <lubiak@proton.me>
2023-12-01 22:45:48 +0100

    1.2

--- 1/main.py
+++ 1/main.py
@@ -1,13 +1,36 @@
+digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 sum = 0
 with open('input.txt', 'r') as input:
 	data = input.read().splitlines()
 	for line in data:
 		num = ''
-		for char in line:
+		for i, char in enumerate(line):
+			loop = True
+			for j, digit in enumerate(digits):
+				if line[i:i+len(digit)] == digit:
+					num += str(j+1)
+					loop = False
+					break
+			if loop == False:
+				break
 			if char.isdigit():
 				num += char
 				break
-		for char in reversed(line):
+		for i, char in enumerate(reversed(line)):
+			loop = True
+			for j, digit in enumerate(digits):
+				if i == 0:
+					if line[-len(digit):] == digit:
+						num += str(j+1)
+						loop = False
+						break
+				else:
+					if line[-len(digit)-i:-i] == digit:
+						num += str(j+1)
+						loop = False
+						break
+			if loop == False:
+				break
 			if char.isdigit():
 				num += char
 				break
